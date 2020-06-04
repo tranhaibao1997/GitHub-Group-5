cc913dcdc2fc3322c7f88ec7eab30ac0bdc8063a
Hai Bao Tran <tranhaibao1997@gmail.com>
2020-06-04 21:41:24 +0700

    add search function

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,27 +1,22 @@
 import React, { useState } from "react";
 import Issue from "./Issue";
 import { Container } from "react-bootstrap";
+import { StoreContext } from "./../ThemeContext";
 
 function IssuesList(props) {
-  let [issueList, setIssueList] = useState(null);
 
-  const getIssueList = async () => {
-    let url = `https://cors-anywhere.herokuapp.com/https://api.github.com/repos/facebook/react/issues?page=1&per_page=20`;
-    let data = await fetch(url);
-    let result = await data.json();
+  let { respName, ownerName,issueList } = React.useContext(StoreContext);
 
-    setIssueList(result);
-  };
+  
   console.log("List:", issueList);
 
   return (
     <div>
-      <button onClick={() => getIssueList()}>Search</button>
       <div className="header">
         <div className="header-wrapper">
           <div className="repo-head">
             <h1>
-                <span></span>
+                <span><i class="fas fa-book"></i></span>
                 <span></span>
                 <span></span>
             </h1>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,52 @@
 import React from "react";
-import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap'
+import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
+import { StoreContext } from "./../ThemeContext";
 
 function NavBar(props) {
+  let name = "";
+  let { respName, ownerName, setIssueList } = React.useContext(StoreContext);
+  function getInputChange(e) {
+    name=e.target.value
+    
+  }
+  const getIssueList = async (ownerName,respName) => {
+    let url = `https://cors-anywhere.herokuapp.com/https://api.github.com/repos/${ownerName}/${respName}/issues?page=1&per_page=20`;
+    let data = await fetch(url);
+    let result = await data.json();
+
+    setIssueList(result);
+  };
+
+  function keyPress(e) {
+    
+    if (e.keyCode == 13) {
+        e.preventDefault();
+        ownerName[1](name.split("/")[0])
+        respName[1](name.split("/")[1])
+        getIssueList(name.split("/")[0],name.split("/")[1])
+    }
+  }
   return (
     <div className="nav-bar">
-      <Navbar  variant="dark">
-        <Navbar.Brand href="#home"><img id="logo" src="github-logo.svg"></img></Navbar.Brand>
+      <Navbar variant="dark">
+        <Navbar.Brand href="#home">
+          <img id="logo" src="github-logo.svg"></img>
+        </Navbar.Brand>
         <Form inline>
-          <FormControl id="navbar-input" type="text" placeholder="Search or jump to..." className="mr-sm-4" />
+          <FormControl
+            onChange={(e) => getInputChange(e)}
+            onKeyDown={(e) => keyPress(e)}
+            id="navbar-input"
+            type="text"
+            placeholder="Search or jump to..."
+            className="mr-sm-4"
+          />
         </Form>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Pull requests</Nav.Link>
           <Nav.Link href="#features">Issues</Nav.Link>
           <Nav.Link href="#pricing">About</Nav.Link>
         </Nav>
-        
       </Navbar>
     </div>
   );
