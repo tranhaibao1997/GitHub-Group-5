5722954d0b5483e5a3b2c2f1d7444eba85ab851e
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-06-04 15:04:59 +0700

    1st push

---
+++ src/components/Issue.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Issue(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Issue;
---
+++ src/components/IssuesList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function IssuesList(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default IssuesList;
