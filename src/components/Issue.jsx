47b32da0bc1cf3be940e5f6b68ff1a0a2b5177e3
ikpak <inkha.pak@gmail.com>
2020-06-04 17:15:25 +0700

    add issue list

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Row, Col} from 'react-bootstrap'
 
 function Issue(props) {
+    let issue = props.issue
+
     return (
-        <div>
-            
-        </div>
+        <Col className="issueContainer">
+            <Row>
+                <h3>{issue.title}</h3>
+            </Row>
+            <Row>
+    <p>#{issue.number} opened {issue.updated_at} by {issue.user.login}</p>
+            </Row>
+        </Col>
     );
 }
 
