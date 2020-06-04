5876f6fdbf50f80b467d60064682d86aab327ae2
Hai Bao Tran <tranhaibao1997@gmail.com>
2020-06-04 21:10:31 +0700

    add nav-bar

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,34 +1,44 @@
-import React, {useState} from 'react';
-import Issue from './Issue'
-import {Container} from 'react-bootstrap'
+import React, { useState } from "react";
+import Issue from "./Issue";
+import { Container } from "react-bootstrap";
 
 function IssuesList(props) {
-    let [issueList, setIssueList] = useState(null)
+  let [issueList, setIssueList] = useState(null);
 
-    const getIssueList = async() => {
-        let url = `https://cors-anywhere.herokuapp.com/https://api.github.com/repos/facebook/react/issues?page=1&per_page=20`
-        let data = await fetch(url)
-        let result = await data.json()
+  const getIssueList = async () => {
+    let url = `https://cors-anywhere.herokuapp.com/https://api.github.com/repos/facebook/react/issues?page=1&per_page=20`;
+    let data = await fetch(url);
+    let result = await data.json();
 
-        setIssueList(result)
-    }
-    console.log("List:", issueList)
+    setIssueList(result);
+  };
+  console.log("List:", issueList);
 
-   
-
-    return (
-        <div>
-            <button onClick={() => getIssueList()}>Search</button>
-            {
-                issueList ? <div>
-                {issueList.map(item => {
-                    return <Container><Issue issue={item} /></Container>
-                })}
-            </div> : <div></div>
-            }
-           
+  return (
+    <div>
+      <button onClick={() => getIssueList()}>Search</button>
+      <div className="header">
+        <div className="header-wrapper">
+          <div className="repo-head">
+            <h1>
+                <span></span>
+                <span></span>
+                <span></span>
+            </h1>
+          </div>
         </div>
-    );
+      </div>
+      {issueList ? (
+        <div>
+          {issueList.map((item) => {
+            return <Issue issue={item} />;
+          })}
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 }
 
 export default IssuesList;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap'
 
 function NavBar(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="nav-bar">
+      <Navbar  variant="dark">
+        <Navbar.Brand href="#home"><img id="logo" src="github-logo.svg"></img></Navbar.Brand>
+        <Form inline>
+          <FormControl id="navbar-input" type="text" placeholder="Search or jump to..." className="mr-sm-4" />
+        </Form>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Pull requests</Nav.Link>
+          <Nav.Link href="#features">Issues</Nav.Link>
+          <Nav.Link href="#pricing">About</Nav.Link>
+        </Nav>
+        
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavBar;
