a74f3895d97565b9a3ae313b342bed9c9b9602fe
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-06-05 14:53:42 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import IssuesList from './components/IssuesList'
-import IssueDetails from './components/IssueDetails'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar';
@@ -12,13 +11,10 @@ function App() {
     <div className="App">
       <NavBar></NavBar>
       <Container>
-        <IssuesList/>
-      </Container>
-      //<IssueDetails ownerName={"fool1280"} respName={"SPOJ"} issueNumber={"1"}/>
-
       <IssuesList />
       <RespList></RespList>
       </Container>
+      
     </div>
   );
 }
