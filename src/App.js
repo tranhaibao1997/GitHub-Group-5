2e1d4fefcc71ded21e19b4c6b7a36d3298a1ec01
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-06-05 16:39:37 +0700

    ???

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/NavBar';
 import {Container} from 'react-bootstrap'
 import RespList from './components/RespList';
+import IssueDetails from './components/IssueDetails';
 
 function App() {
   return (
     <div className="App">
+      
       <NavBar></NavBar>
       <Container>
+      {/* <IssueDetails  ownerName={"fool1280"} respName={"SPOJ"} issueNumber={"1"}></IssueDetails> */}
       <IssuesList />
       <RespList></RespList>
       </Container>
